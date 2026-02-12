a77435895cb42d08f8fc37095a07b18e354bbc64
Romano Giannetti <romano.giannetti@gmail.com>
2026-02-12 19:38:59 +0100

    Add enverb.sty to make Travis happy
    and texlive-plain-generic...

--- tools/check.sh
+++ tools/check.sh
@@ -3,6 +3,10 @@ set -e # Exit with nonzero exit code if anything fails
 
 SOURCE_BRANCH="master"
 
+if [[ -f tools/enverb.sty ]]; then
+  cp tools/enverb.sty doc/
+fi
+
 # Pull requests and commits to other branches shouldn't try to deploy, just build to verify
 if [ "$TRAVIS_PULL_REQUEST" != "false" -o "$TRAVIS_BRANCH" != "$SOURCE_BRANCH" ]; then
 	echo "Will skip deploy; just do a compile check."
